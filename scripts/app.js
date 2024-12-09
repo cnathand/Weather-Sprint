a524fccb34a5de843b82b5c7c4cedfe2f9868b16
cnathand <cdechyothin@gmail.com>
2024-12-09 15:17:54 -0800

    set up the api

--- scripts/app.js
+++ scripts/app.js
@@ -1,41 +1,46 @@
-import { APIKEY } from '../scripts/enviroment.js';   
+const API_KEY = "42fe14fb866c5a52fac4ac66bfaf815a"; // Your API key
 
+// Define latitude and longitude
+const lat = 37.961632; // Replace with desired latitude
+const lon = -121.275604; // Replace with desired longitude
 
-//Geo location is a built in API that allows the user to share there location apon request.
+// Async function to fetch weather data
+async function fetchWeather() {
 
-//navigator.geolocation this return geolocation object
-//getCurrentPosition method lets the web app get the current position
-
-navigator.geolocation.getCurrentPosition(success, errorFunc);
-//You can think of this as an if statment if user accepts we run success else we run errorFunc
-
-//Example of the geolocation Object below
-{
-    coords: {
-        latitude: 32.1234;
-        longitude: 13.1234;
-    }
-}
-
-//If the user accepts we run success function
-function success(position){
-    console.log(position);
-    let lat = position.coords.latitude;
-    let long = position.coords.longitude;
-    console.log("Our latitude: " + lat);
-    console.log("Our longitude: " + long);
-    console.log("We know where you are!");
-    currentLocation(lat,long);
-}
-//If the user denies we run errorFunc
-function errorFunc(error){
-    console.log(error.message);
-}
-
-async function currentLocation(lat,long) {
-    const response = await fetch(`https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${long}&appid=42fe14fb866c5a52fac4ac66bfaf815a`)
+    const url = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&units=imperial&appid=${API_KEY}`;
+    const response = await fetch(url);
     const data = await response.json();
-    console.log(data);
-};  
 
-currentLocation();
+    console.log("Monday");
+    console.log("city: ", data.city.name);
+    console.log(`temp: ${data.list[0].main.temp}°F`)
+    console.log(`highest: ${data.list[0].main.temp_max}°F`)
+    console.log(`lowest: ${data.list[0].main.temp_min}°F`)
+
+
+    console.log("Saturday");
+    console.log("city: ", data.city.name);
+    console.log(`temp: ${data.list[8].main.temp}°F`)
+    console.log(`highest: ${data.list[8].main.temp_max}°F`)
+    console.log(`lowest: ${data.list[8].main.temp_min}°F`)
+
+    console.log("Sunday");
+    console.log("city: ", data.city.name);
+    console.log(`temp: ${data.list[16].main.temp}°F`)
+    console.log(`highest: ${data.list[16].main.temp_max}°F`)
+    console.log(`lowest: ${data.list[16].main.temp_min}°F`)
+
+    console.log("Monday");
+    console.log("city: ", data.city.name);
+    console.log(`temp: ${data.list[24].main.temp}°F`)
+    console.log(`highest: ${data.list[24].main.temp_max}°F`)
+    console.log(`lowest: ${data.list[24].main.temp_min}°F`)
+
+    console.log("Tuesday");
+    console.log("city: ", data.city.name);
+    console.log(`temp: ${data.list[32].main.temp}°F`)
+    console.log(`highest: ${data.list[32].main.temp_max}°F`)
+    console.log(`lowest: ${data.list[32].main.temp_min}°F`)
+}
+
+fetchWeather();
