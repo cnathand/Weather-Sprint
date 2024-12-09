61cf2d9d979919c6a6d7639498ac266cc5f510dc
cnathand <cdechyothin@gmail.com>
2024-12-09 14:26:22 -0800

    finshed rough draft html css. starting JS

--- scripts/app.js
+++ scripts/app.js
@@ -1,4 +1,4 @@
-import { APIKEY } from './environment.js';   
+import { APIKEY } from '../scripts/enviroment.js';   
 
 
 //Geo location is a built in API that allows the user to share there location apon request.
@@ -20,15 +20,22 @@ navigator.geolocation.getCurrentPosition(success, errorFunc);
 //If the user accepts we run success function
 function success(position){
     console.log(position);
-    console.log("Our latitude: " + position.coords.latitude);
-    console.log("Our longitude: " + position.coords.longitude);
+    let lat = position.coords.latitude;
+    let long = position.coords.longitude;
+    console.log("Our latitude: " + lat);
+    console.log("Our longitude: " + long);
     console.log("We know where you are!");
-
+    currentLocation(lat,long);
 }
-
 //If the user denies we run errorFunc
 function errorFunc(error){
     console.log(error.message);
 }
 
+async function currentLocation(lat,long) {
+    const response = await fetch(`https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${long}&appid=42fe14fb866c5a52fac4ac66bfaf815a`)
+    const data = await response.json();
+    console.log(data);
+};  
 
+currentLocation();
